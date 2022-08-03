e9ceab12ddfe487d45d78e37974c9bdcff181280
Oliver343 <oliver_343@hotmail.com>
2022-08-03 10:05:47 +0200

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7d9103bd-1f31-4bd6-8515-50ba1b4fdf7f';
+export const MODAL_CLOSE_SEC = 2.5;
